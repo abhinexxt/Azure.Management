27c590c500cd6669574c448a302bb38d95c3e1a5
anuruvakkunnath <anuruvakkunnath@beckman.com>
2022-05-31 15:29:51 +0200

    INFRA: Initialize CloudBroker

---
+++ Azure.Management/Azure.Management.Provision/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Azure.Management.Provision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
