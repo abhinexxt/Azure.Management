724d17873b4b33dea0da2f22bc1fcbcdffe44024
anuruvakkunnath <anuruvakkunnath@beckman.com>
2022-05-31 20:36:36 +0200

    BROKERS: LoggingBroker implementation

---
+++ Azure.Management/Azure.Management.Provision/Brokers/Loggings/LoggingBroker.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Azure.Management.Provision.Brokers.Loggings
+{
+    public class LoggingBroker : ILoggingBroker
+    {
+        public void LogActivity(string message) => 
+            Console.WriteLine(message);
+        
+    }
+}
