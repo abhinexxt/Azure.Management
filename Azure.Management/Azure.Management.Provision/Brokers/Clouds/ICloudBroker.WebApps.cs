14172019d8e7ef1b289d48764b0ab21b94036221
anuruvakkunnath <anuruvakkunnath@beckman.com>
2022-05-31 19:57:35 +0200

    BROKERS: Implemented CreateWebAppAsync

---
+++ Azure.Management/Azure.Management.Provision/Brokers/Clouds/ICloudBroker.WebApps.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Azure.Management.AppService.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+
+namespace Azure.Management.Provision.Brokers.Clouds
+{
+    public partial interface ICloudBroker
+    {
+        ValueTask<IWebApp> CreateWebAppAsync(
+            string webAppName,
+            string connectionString,
+            IAppServicePlan appServicePlan,
+            IResourceGroup resourceGroup);
+    }
+}
